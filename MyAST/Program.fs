b4df3c14229657c8a29cdcf56b2199a2253b30f6
Steven Dunn <speed10152003@gmail.com>
2025-05-02 15:58:33 -0500

    Part of JsSyntaxtree files

--- MyAST/Program.fs
+++ MyAST/Program.fs
@@ -1,13 +1,21 @@
 open System
+open Giraffe
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddGiraffe() |> ignore
     let app = builder.Build()
+    app.UseStaticFiles() |> ignore
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    let webApp = 
+        choose [
+            route "/" >=> htmlFile "./wwwroot/index.html"
+        ]
+    
+    app.UseGiraffe(webApp)
 
     app.Run()
 
